4962f4911b094ad30e2da7217b514f4642d32dd0
Leonan <pedroleonan85@gmail.com>
2023-05-20 21:29:27 -0300

    Identação

--- script.js
+++ script.js
@@ -1,77 +1,77 @@
 
-const sonic = document.querySelector('.sonic');
-sonic.classList.add('paused');
+const sonic = document.querySelector('.sonic')
+sonic.classList.add('paused')
 
-const monster = document.querySelector('.monster');
-monster.classList.add('paused');
+const monster = document.querySelector('.monster')
+monster.classList.add('paused')
 
-const cloud = document.querySelector('.clouds');
-cloud.classList.add('paused');
+const cloud = document.querySelector('.clouds')
+cloud.classList.add('paused')
 
-const ring = document.querySelector('.ring');
-ring.classList.add('paused');
+const ring = document.querySelector('.ring')
+ring.classList.add('paused')
 
-const contador = document.querySelector('.contador p');
-contador.classList.add('paused');
+const contador = document.querySelector('.contador p')
+contador.classList.add('paused')
 
 const fp = document.querySelector('.first-page')
 
 function runGame() {
 
 
-    sonic.classList.remove('paused');
-    monster.classList.remove('paused');
-    cloud.classList.remove('paused');
-    ring.classList.remove('paused');
-    contador.classList.remove('paused');
+    sonic.classList.remove('paused')
+    monster.classList.remove('paused')
+    cloud.classList.remove('paused')
+    ring.classList.remove('paused')
+    contador.classList.remove('paused')
   
-    let valor = 0;
-    let contIncre = false;
-    let teste;
-    let onAir;
-    let isDouble;
-    let onFall;
+    let valor = 0
+    let contIncre = false
+    let teste
+    let onAir
+    let isDouble
+    let onFall
   
     function jump() {
         if(!sonic.classList.contains('game-over')){
       sonic.classList.add('jump')}
-      onAir = true;
+      onAir = true
       setTimeout(() => {
-        sonic.classList.remove('jump');
-        onAir = false;
-      }, 800);
+        sonic.classList.remove('jump')
+        onAir = false
+      }, 800)
     }
   
     function doublejump() {
-      sonic.classList.remove('jump');
-      sonic.classList.add('doublejump');
-      isDouble = true;
+      sonic.classList.remove('jump')
+      sonic.classList.add('doublejump')
+      isDouble = true
       setTimeout(() => {
         sonic.classList.add('fall');
-        sonic.classList.remove('doublejump');
+        sonic.classList.remove('doublejump')
         isDouble = false;
         setTimeout(() => {
-          sonic.classList.remove('fall');
-        }, 300);
-      }, 400);
+          sonic.classList.remove('fall')
+        }, 300)
+      }, 400)
     }
   
     function fall() {
-      sonic.classList.remove('jump');
-      sonic.classList.remove('doublejump');
-      sonic.classList.add('forcedfall');
-      onFall = true;
+      sonic.classList.remove('jump')
+      sonic.classList.remove('doublejump')
+      sonic.classList.add('forcedfall')
+      onFall = true
       setTimeout(() => {
-        sonic.classList.remove('forcedfall');
-        onFall = false;
-      }, 200);
+        sonic.classList.remove('forcedfall')
+        onFall = false
+      }, 200)
     }
   
     document.addEventListener('keyup', (event) => {
       if (event.key === 'ArrowUp' && !onAir && !onFall) {
-        jump();
+        jump()
       }
-    });
+    })
   
     document.addEventListener('keyup', (event) => {
       if (
@@ -81,9 +81,9 @@ function runGame() {
         !sonic.classList.contains('fall') &&
         !sonic.classList.contains('forcedfall')
       ) {
-        doublejump();
+        doublejump()
       }
-    });
+    })
   
     document.addEventListener('keyup', (event) => {
       if (
@@ -91,14 +91,14 @@ function runGame() {
         !sonic.classList.contains('game-over') &&
         (sonic.classList.contains('doublejump') || sonic.classList.contains('jump'))
       ) {
-        fall();
+        fall()
       }
-    });
+    })
   
     function checkCollision() {
-      const sonicPosition = +window.getComputedStyle(sonic).top.replace('px', '');
-      const ringPosition = +window.getComputedStyle(ring).top.replace('px', '');
-      const ringLeft = ring.offsetLeft;
+      const sonicPosition = +window.getComputedStyle(sonic).top.replace('px', '')
+      const ringPosition = +window.getComputedStyle(ring).top.replace('px', '')
+      const ringLeft = ring.offsetLeft
   
       if (
         sonicPosition >= ringPosition - 80 &&
@@ -109,92 +109,92 @@ function runGame() {
         if (!contIncre) {
             ++valor
             let valorFormatado = valor.toString().padStart(4, '0')
-          contador.innerHTML = `${valorFormatado}`;
-          ring.style.display = 'none';
-          contIncre = true; // Marcando que o contador foi incrementado
+          contador.innerHTML = `${valorFormatado}`
+          ring.style.display = 'none'
+          contIncre = true
         }
       } else {
-        contIncre = false; // Resete a variável de controle quando não houver colisão
+        contIncre = false
       }
     }
   
     function setTop() {
       const sonicPosition = sonic.offsetTop;
-      sonic.style.setProperty('--top-atual', `${sonicPosition - 2}px`);
-      sonic.style.setProperty('--top-final', `${sonicPosition - 5}px`);
-      sonic.style.setProperty('--top-meio', `${sonicPosition + 10}px`);
+      sonic.style.setProperty('--top-atual', `${sonicPosition - 2}px`)
+      sonic.style.setProperty('--top-final', `${sonicPosition - 5}px`)
+      sonic.style.setProperty('--top-meio', `${sonicPosition + 10}px`)
     }
   
     const loop = setInterval(() => {
-      const monsterPosition = monster.offsetLeft;
-      const sonicPosition = +window.getComputedStyle(sonic).top.replace('px', '');
-      const cloudPosition = cloud.offsetLeft;
-      const ringPosition = ring.offsetLeft;
+      const monsterPosition = monster.offsetLeft
+      const sonicPosition = +window.getComputedStyle(sonic).top.replace('px', '')
+      const cloudPosition = cloud.offsetLeft
+      const ringPosition = ring.offsetLeft
   
       if (monsterPosition <= 170 && monsterPosition > 0 && sonicPosition > 150) {
-        monster.style.animation = 'none';
-        monster.style.left = `${monsterPosition}px`;
+        monster.style.animation = 'none'
+        monster.style.left = `${monsterPosition}px`
   
         sonic.classList.add('game-over');
+
+        sonic.style.animation = 'sonic-over 0.7s linear forwards'
+        sonic.style.top = `${sonicPosition}px`
   
-        sonic.style.animation = 'sonic-over 0.7s linear forwards';
-        sonic.style.top = `${sonicPosition}px`;
+        cloud.style.animation = 'none'
+        cloud.style.left = `${cloudPosition}px`
   
-        cloud.style.animation = 'none';
-        cloud.style.left = `${cloudPosition}px`;
-  
-        ring.style.animation = 'ringEnd 0.3s ease forwards';
+        ring.style.animation = 'ringEnd 0.3s ease forwards'
         setTimeout(() => {
-          ring.remove();
-        }, 100);
-        ring.style.left = `${ringPosition}px`;
+          ring.remove()
+        }, 100)
+        ring.style.left = `${ringPosition}px`
   
-        clearInterval(loop);
-        clearInterval(ringReloadInterval);
+        clearInterval(loop)
+        clearInterval(ringReloadInterval)
         document.addEventListener('keyup', (event) => {
           setTimeout(()=>{
             if (event.key === 'ArrowUp') {
             document.location.reload()
           }
         }, 1200)})
-        teste = false;
+        teste = false
       }
   
-      setTop();
-  
-      checkCollision();
+      setTop()
+
+      checkCollision()
     }, 10);
   
     function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min) + min);
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min) + min)
     }
   
-    let ringReloadInterval;
+    let ringReloadInterval
   
     function startRingReload() {
-      clearInterval(ringReloadInterval); // Limpa o intervalo anterior, se existir
+      clearInterval(ringReloadInterval)
       ringReloadInterval = setInterval(() => {
-        ring.style.top = `${getRandomInt(0, 350)}px`;
+        ring.style.top = `${getRandomInt(0, 350)}px`
         ring.style.display = 'block';
-      }, 2000);
+      }, 2000)
     }
-    startRingReload();
+    startRingReload()
   
     ring.addEventListener('animationend', () => {
-      ring.style.removeProperty('left'); // Remove a propriedade 'left'
-      ring.style.removeProperty('top'); // Remove a propriedade 'top'
-      ring.style.removeProperty('display'); // Remove a propriedade 'display'
+      ring.style.removeProperty('left')
+      ring.style.removeProperty('top')
+      ring.style.removeProperty('display')
       ring.style.removeProperty('animation');
   
       if (teste) {
         setTimeout(() => {
-          ring.style.animation = 'ring-animation 3s linear forwards'; // Inicia novamente a animação do anel
-          startRingReload(); // Reinicia o intervalo para exibir o próximo anel
-        }, 1000); // Aguarda 100ms antes de reiniciar a animação para evitar problemas de renderização
+          ring.style.animation = 'ring-animation 3s linear forwards'
+          startRingReload()
+        }, 1000)
       }
-    });
+    })
   }
 
   if (sonic.classList.contains('paused')) {
